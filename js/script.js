e69e751019f5456db2f00725a015710855ac6b6f
Amnon Carmi <ace7884@hotmail.com>
2022-04-21 15:26:50 +0300

    First milestone complete

--- js/script.js
+++ js/script.js
@@ -1,4 +1,25 @@
+// Milestone 1 global variables
+let X = 8;
+let Y = 0;
+let display = document.getElementsByTagName("label")[0];
 
-let userInput =  document.getElementsByClassName('input');
-let button = document.getElementsByClassName('button');
+// takes X input outputs X's Fibonacci as Y
+function fibonacciCalc(X) {
+  let prevNum = X - 1;
+  Y += X + prevNum;
+  return Y;
+}
 
+// add to Html Result
+function addToHtmlY(){
+  fibonacciCalc(X);
+  display = display.innerText += ` ${X} is ${Y}`;
+}
+
+addToHtmlY();
+
+
+// future milestone variables
+let userInput = document.getElementById("userInput");
+let button = document.getElementsByClassName("btn")[0];
+let questionOutput = document.getElementById("inputResult");
