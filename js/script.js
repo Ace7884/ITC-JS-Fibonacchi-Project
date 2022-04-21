bc9d850e1dccc04166d63942a5255208f003af29
Amnon Carmi <ace7884@hotmail.com>
2022-04-21 20:24:14 +0300

    Milestone 2 done

--- js/script.js
+++ js/script.js
@@ -1,30 +1,47 @@
 // Milestone 1 global variables
-let X = 5;
-let Y = 8;
+let X = 23;
+let Y = 6;
 let display = document.getElementsByTagName("label")[0];
 
-// add to Html Result
-function addToHtmlY(){
+// Appends Y to Html
+function addToHtmlY() {
   display = display.innerText += ` ${X} is ${Y}`;
 }
-addToHtmlY();
 
 
-
-
-
-/*milestone 2 work in progress ignore untill posted
+//milestone 2
+//starter fibonacci set conditionals
+function fibonacciStartSet(X) {
+  if (X === 0) {
+    return (Y = 0);
+  }
+  if (X < 3 && X > 0) {
+    return (Y = 1);
+  }
+}
 
 // takes X input outputs X's Fibonacci as Y
 function fibonacciCalc(X) {
-  let prevNum = X - 1;
-  Y += X + prevNum;
+  Y = 1;
+  let numSum = 0;
+  let currentNum = 1;
+  let secondNum = 1;
+
+  fibonacciStartSet(X);
+
+  for (let i = 2; i < X; i++) {
+    numSum = currentNum + secondNum;
+    Y = numSum;
+    secondNum = currentNum;
+    currentNum = numSum;
+  }
   return Y;
 }
 
+fibonacciCalc(X);
+addToHtmlY();
 
 // future milestone variables
 let userInput = document.getElementById("userInput");
 let button = document.getElementsByClassName("btn")[0];
 let questionOutput = document.getElementById("inputResult");
-*/
