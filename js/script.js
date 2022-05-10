e7a627457453ecccaff65133d3e5266095049901
Amnon Carmi <ace7884@hotmail.com>
2022-05-10 15:41:23 +0300

    Milestone 4 Complete final

--- js/script.js
+++ js/script.js
@@ -1,79 +1,10 @@
+//Milestone 4
+
 //global variables
-let X = 23;
-let Y = 6;
+let output = "";
 let display = document.getElementsByTagName("label")[0];
 let button1 = document.querySelectorAll("button")[0];
 let questionOutput = document.querySelector("strong");
-
-//Milestone 1
-
-//Milestone 1 functions
-//Appends Y to Html
-// function addToHtmlY() {
-//   display = display.innerText += ` ${X} is ${Y}`;
-// }
-
-//Milestone 2
-
-//Milestone 2 functions
-//Sets Starter fibonacci set conditionals
-// function fibonacciStartSet(X) {
-//   if (X === 0) {
-//     return (Y = 0);
-//   }
-//   if (X < 3 && X > 0) {
-//     return (Y = 1);
-//   }
-// }
-
-//Takes X input outputs X's Fibonacci as Y
-// function fibonacciCalc(X) {
-//   Y = 0;
-//   let numSum = 0;
-//   let currentNum = 1;
-//   let secondNum = 1;
-
-//   fibonacciStartSet(X);
-
-//   for (let i = 2; i < X; i++) {
-//     numSum = currentNum + secondNum;
-//     Y = numSum;
-//     secondNum = currentNum;
-//     currentNum = numSum;
-//   }
-//   return Y;
-// }
-
-//Milestone 3
-
-//Milestone 3 functions
-
-// //Register userinput to X and display Y
-// function registerInput() {
-//   X = document.getElementById("userInput").value;
-//   //replaced with recursion function
-//   fibonacciCalcRecursion(X)
-//   displayY(Y);
-//   setTimeout(() => {
-//     document.getElementsByClassName('inputResult')[0].classList.add('d-none');
-//   }, 5000);
-//   Y=0;
-// }
-
-//Milestone 3* recursion version for calc instead
-
-// function fibonacciCalcRecursion(X) {
-//   if (X > 0 && X < 3) {
-//     return (Y = 1);
-//   }
-//   if (X === 0) {
-//     return (Y = 0);
-//   }
-//   Y = fibonacciCalcRecursion(X - 1) + fibonacciCalcRecursion(X - 2);
-//   return Y;
-// }
-
-//Milestone 4
 
 //on button click register Input and calculate Fibonacci
 button1.addEventListener("click", registerInput);
@@ -81,13 +12,13 @@ document.getElementsByClassName("inputResult")[0].classList.add("d-none");
 
 //Revised function to replace previous fibonacci calc and display to local server execution
 function registerInput() {
-  X = document.getElementById("userInput").value;
+  let input = document.getElementById("userInput").value;
   //server doesnt accept zero thus condition it
-  if (X == 0 ) {
+  if (input == 0) {
     displayY(0);
-  }else{
+  } else {
     //replace calc to local server
-    callServer(X);
+    callServer(input);
   }
 }
 
@@ -97,13 +28,16 @@ function callServer(num) {
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      Y = data.result;
-      displayY(Y);
+      output = data.result;
+      displayY(output);
     });
 }
 
-//Displays Fibonacci Number
+//Displays Fibonacci Number and timeout after awhile
 function displayY(num) {
   questionOutput.innerText = `${num}`;
   document.getElementsByClassName("inputResult")[0].classList.remove("d-none");
+  setTimeout(() => {
+        document.getElementsByClassName('inputResult')[0].classList.add('d-none');
+      }, 8000);
 }
